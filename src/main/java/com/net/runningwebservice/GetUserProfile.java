f4fb57d2772f3df0f13856031cd04be0a9c326b3
pinkjo8825 <83690209+pinkjo8825@users.noreply.github.com>
2024-04-11 04:38:21 +0700

    add details to getProfile

--- src/main/java/com/net/runningwebservice/GetUserProfile.java
+++ src/main/java/com/net/runningwebservice/GetUserProfile.java
@@ -1,5 +1,6 @@
 package com.net.runningwebservice;
 
+import com.net.running_web_service.GetRecommendEventResponse;
 import com.net.running_web_service.GetUserProfileRequest;
 import com.net.running_web_service.GetUserProfileResponse;
 import io.jsonwebtoken.Claims;
@@ -10,6 +11,7 @@ import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntDocumentManager;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.rulesys.GenericRuleReasonerFactory;
@@ -20,10 +22,16 @@ import org.apache.jena.vocabulary.ReasonerVocabulary;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GetUserProfile {
     public static GetUserProfileResponse run(GetUserProfileRequest request) {
         GetUserProfileResponse response = new GetUserProfileResponse();
+
+        ArrayList<String> recEvents = new ArrayList<String>();
 
         String NS = SharedConstants.NS;
         String output_filename = SharedConstants.output_filename;
@@ -117,12 +125,13 @@ public class GetUserProfile {
 
             StmtIterator i1 = inf.listStatements(a, p, (RDFNode) null);
 
+
+
             while (i1.hasNext()) {
-                GetUserProfileResponse.RunningEvent event = new GetUserProfileResponse.RunningEvent();
                 Statement statement = i1.nextStatement();
 
                 String statementString = statement.getObject().toString();
-                System.out.println(statementString);
+//                System.out.println(statementString);
                 Resource re = data.getResource(statementString);
                 StmtIterator i2 = inf.listStatements(re, c, (RDFNode) null);
                 int conf = 0;
@@ -137,20 +146,103 @@ public class GetUserProfile {
                         continue;
                     }
                     int roundedConfValue = (int) Math.round(confValue);
-                    System.out.println("=> " + roundedConfValue);
+//                    System.out.println("=> " + roundedConfValue);
                     if (roundedConfValue > conf) {
                         conf = roundedConfValue;
                     }
                 }
                 String[] parts = statementString.split("#");
                 String extractedName = parts[1];
+                recEvents.add(extractedName);
 
-                event.setRunningEventName(extractedName);
 
-                response.getRunningEvent().add(event);
-                System.out.println(conf);
             }
         }
+        ArrayList<String> formattedEventNames = SharedConstants.formatEventNames(recEvents);
+
+        String filterClause = formattedEventNames.stream()
+                .map(eventName -> "?eventName = \"" + eventName + "\"")
+                .collect(Collectors.joining(" || ", "FILTER (", ") ."));
+
+        Model dataOnto = RDFDataMgr.loadModel("file:" + SharedConstants.ontologyPath);
+
+        String queryString  = """
+                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                PREFIX re: <http://www.semanticweb.org/guind/ontologies/runningeventontology#>
+
+                SELECT ?eventName ?district ?raceTypeName ?typeOfEvent ?price ?organizationName ?activityArea ?standardOfEvent ?levelOfEvent ?startPeriod ?reward
+                WHERE {
+                  ?event rdf:type re:RunningEvent .
+                  ?event re:RunningEventName ?eventName .
+                  ?event re:hasEventVenue ?venue .
+                  ?venue re:District ?district .
+                  ?event re:TypeOfEvent ?typeOfEvent .
+                  ?event re:hasRaceType ?raceType .
+                  ?raceType re:RaceTypeName ?raceTypeName .
+                  ?raceType re:ActivityArea ?activityArea .
+                  ?raceType re:Price ?price .
+                  ?raceType re:StartPeriod ?startPeriod .
+                  ?raceType re:Reward ?reward .
+                  ?event re:isOrganizedBy ?organization .
+                  ?organization re:OrganizationName ?organizationName .
+                  ?event re:StandardOfEvent ?standardOfEvent .
+                  ?event re:LevelOfEvent ?levelOfEvent .
+                     
+                """ + filterClause  + "}";
+//        System.out.println(queryString);
+
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qexec = QueryExecutionFactory.create(query, dataOnto);
+        ResultSet resultSet = qexec.execSelect();
+
+        try {
+            Map<String, GetUserProfileResponse.RunningEvent> eventsMap = new HashMap<>();
+
+            while (resultSet.hasNext()) {
+                QuerySolution solution = resultSet.nextSolution();
+
+                String eventName = solution.getLiteral("eventName").getString().trim();
+                GetUserProfileResponse.RunningEvent event = eventsMap.computeIfAbsent(eventName, k -> {
+                    GetUserProfileResponse.RunningEvent newEvent = new GetUserProfileResponse.RunningEvent();
+                    newEvent.setRunningEventName(eventName);
+                    newEvent.setDistrict(solution.getLiteral("district").getString().trim());
+                    newEvent.setTypeofEvent(solution.getLiteral("typeOfEvent").getString().trim());
+                    newEvent.setOrganization(solution.getLiteral("organizationName").getString().trim());
+                    newEvent.setActivityArea(solution.getLiteral("activityArea").getString().trim());
+                    newEvent.setStandard(solution.getLiteral("standardOfEvent").getString().trim());
+                    newEvent.setLevel(solution.getLiteral("levelOfEvent").getString().trim());
+                    newEvent.setStartPeriod(solution.getLiteral("startPeriod").getString().trim());
+                    newEvent.setPrices(new GetUserProfileResponse.RunningEvent.Prices());
+                    newEvent.getPrices().getPrice().clear();
+                    newEvent.setRaceTypes(new GetUserProfileResponse.RunningEvent.RaceTypes());
+                    newEvent.getRaceTypes().getRaceType().clear();
+                    newEvent.setRewards(new GetUserProfileResponse.RunningEvent.Rewards());
+                    newEvent.getRewards().getReward().clear();
+                    return newEvent;
+                });
+
+                String raceType = solution.getLiteral("raceTypeName").getString().trim();
+                if (!event.getRaceTypes().getRaceType().contains(raceType)) {
+                    event.getRaceTypes().getRaceType().add(raceType);
+                }
+
+                String price = solution.getLiteral("price").getString().trim();
+                if (!event.getPrices().getPrice().contains(price)) {
+                    event.getPrices().getPrice().add(price);
+                }
+
+                String rewardName = solution.getLiteral("reward").getString().trim();
+                if (!event.getRewards().getReward().contains(rewardName)) {
+                    event.getRewards().getReward().add(rewardName);
+                }
+            }
+            response.getRunningEvent().addAll(eventsMap.values());
+
+        } finally {
+            qexec.close();
+        }
+
         return response;
     }
 }
