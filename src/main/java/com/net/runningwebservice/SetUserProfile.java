568a49a0a10c643f133e29445ab90a22576c80ce
pinkjo8825 <83690209+pinkjo8825@users.noreply.github.com>
2024-04-28 16:35:44 +0700

    prevent creat duplicate username

--- src/main/java/com/net/runningwebservice/SetUserProfile.java
+++ src/main/java/com/net/runningwebservice/SetUserProfile.java
@@ -3,6 +3,10 @@ package com.net.runningwebservice;
 import com.net.running_web_service.SetUserProfileRequest;
 import com.net.running_web_service.SetUserProfileResponse;
 import org.apache.jena.ontology.*;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.vocabulary.RDF;
@@ -23,77 +27,96 @@ public class SetUserProfile {
         dm.addAltEntry("http://www.semanticweb.org/guind/ontologies/runningeventontology",
                 "file:" + ontologyPath);
         m.read("http://www.semanticweb.org/guind/ontologies/runningeventontology", "RDF/XML");
-        OntClass userClass = m.getOntClass(NS + "User");
-        OntProperty userActivityArea = m.getDatatypeProperty(NS + "ActivityAreaInterest");
-        OntProperty userStartPeriod = m.getDatatypeProperty(NS + "StartPeriodInterest");
-        OntProperty userReward = m.getDatatypeProperty(NS + "RewardInterest");
-        OntProperty hasRacetype = m.getObjectProperty(NS + "hasRaceTypeInterest");
-        OntProperty hasOrganization = m.getObjectProperty(NS + "hasOrganizationInterest");
-        OntProperty userLocation = m.getDatatypeProperty(NS + "LocationInterest");
-        OntProperty userTypeOfEvent = m.getDatatypeProperty(NS + "TypeOfEventInterest");
-        OntProperty userEventPrice = m.getDatatypeProperty(NS + "EventPriceInterest");
-        OntProperty userLevelEvent = m.getDatatypeProperty(NS + "LevelEventInterest");
-        OntProperty userStandardEvent = m.getDatatypeProperty(NS + "StandardEventInterest");
-        OntProperty userAge = m.getDatatypeProperty(NS + "UserAge");
-        OntProperty userName = m.getDatatypeProperty(NS + "Username");
-        OntProperty userNationality = m.getDatatypeProperty(NS + "UserNationality");
-        OntProperty userSex = m.getDatatypeProperty(NS + "UserSex");
-        OntProperty password = m.getDatatypeProperty(NS + "Password");
 
+        Boolean exist = true;
+        String queryStr = String.format(
+                "ASK { ?user <%sUsername> \"%s\" . }",
+                NS,  request.getUsername()
+        );
+        System.out.println(queryStr);
 
-
-        String userProfileName = request.getUsername();
-        Resource userInstance = m.createResource(NS + userProfileName);
-
-        Byte ageReg = request.getAge();
-        String nationalityReg = request.getNationality();
-        String genderReg = request.getGender();
-        String districtReg = request.getDistrict();
-        String raceTypeReg = request.getRaceType();
-        String typeofEventReg = request.getTypeofEvent();
-        String priceReg = request.getPrice();
-        String organizationReg = request.getOrganization();
-        String activityAreaReg = request.getActivityArea();
-        String standardReg = request.getStandard();
-        String levelReg = request.getLevel();
-        String startPeriodReg = request.getStartPeriod();
-        String rewardReg = request.getReward();
-        String plainPassword = request.getPassword();
-
-        userInstance.addProperty(RDF.type, userClass);
-        userInstance.addProperty(userName, userProfileName);
-        userInstance.addProperty(userAge, String.valueOf(ageReg));
-        userInstance.addProperty(userNationality, nationalityReg);
-        userInstance.addProperty(userSex, genderReg);
-        userInstance.addProperty(userLocation, districtReg);
-        userInstance.addProperty(hasRacetype, raceTypeReg);
-        userInstance.addProperty(userTypeOfEvent, typeofEventReg);
-        userInstance.addProperty(userEventPrice, priceReg);
-        userInstance.addProperty(hasOrganization, organizationReg);
-        userInstance.addProperty(userActivityArea, activityAreaReg);
-        userInstance.addProperty(userStandardEvent, standardReg);
-        userInstance.addProperty(userLevelEvent, levelReg);
-        userInstance.addProperty(userStartPeriod, startPeriodReg);
-        userInstance.addProperty(userReward, rewardReg);
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String hashedPassword = encoder.encode(plainPassword);
-
-        userInstance.addProperty(password, hashedPassword);
-
-
-
-
-        try (FileOutputStream out = new FileOutputStream(ontologyPath)) {
-            m.write(out, "RDF/XML");
-            System.out.println(userInstance);
-        } catch (IOException e) {
-            e.printStackTrace();
+        Query query = QueryFactory.create(queryStr);
+        QueryExecution qexec = QueryExecutionFactory.create(query, m);
+        try {
+            exist =  qexec.execAsk();
+        } finally {
+            qexec.close();
         }
-        System.out.println("Number of statements in OntModel: " + m.size());
-        response.setStatus("Success");
 
-        return response;
+        if(!exist) {
+            OntClass userClass = m.getOntClass(NS + "User");
+            OntProperty userActivityArea = m.getDatatypeProperty(NS + "ActivityAreaInterest");
+            OntProperty userStartPeriod = m.getDatatypeProperty(NS + "StartPeriodInterest");
+            OntProperty userReward = m.getDatatypeProperty(NS + "RewardInterest");
+            OntProperty hasRacetype = m.getObjectProperty(NS + "hasRaceTypeInterest");
+            OntProperty hasOrganization = m.getObjectProperty(NS + "hasOrganizationInterest");
+            OntProperty userLocation = m.getDatatypeProperty(NS + "LocationInterest");
+            OntProperty userTypeOfEvent = m.getDatatypeProperty(NS + "TypeOfEventInterest");
+            OntProperty userEventPrice = m.getDatatypeProperty(NS + "EventPriceInterest");
+            OntProperty userLevelEvent = m.getDatatypeProperty(NS + "LevelEventInterest");
+            OntProperty userStandardEvent = m.getDatatypeProperty(NS + "StandardEventInterest");
+            OntProperty userAge = m.getDatatypeProperty(NS + "UserAge");
+            OntProperty userName = m.getDatatypeProperty(NS + "Username");
+            OntProperty userNationality = m.getDatatypeProperty(NS + "UserNationality");
+            OntProperty userSex = m.getDatatypeProperty(NS + "UserSex");
+            OntProperty password = m.getDatatypeProperty(NS + "Password");
+
+
+            String userProfileName = request.getUsername();
+            Resource userInstance = m.createResource(NS + userProfileName);
+
+            Byte ageReg = request.getAge();
+            String nationalityReg = request.getNationality();
+            String genderReg = request.getGender();
+            String districtReg = request.getDistrict();
+            String raceTypeReg = request.getRaceType();
+            String typeofEventReg = request.getTypeofEvent();
+            String priceReg = request.getPrice();
+            String organizationReg = request.getOrganization();
+            String activityAreaReg = request.getActivityArea();
+            String standardReg = request.getStandard();
+            String levelReg = request.getLevel();
+            String startPeriodReg = request.getStartPeriod();
+            String rewardReg = request.getReward();
+            String plainPassword = request.getPassword();
+
+            userInstance.addProperty(RDF.type, userClass);
+            userInstance.addProperty(userName, userProfileName);
+            userInstance.addProperty(userAge, String.valueOf(ageReg));
+            userInstance.addProperty(userNationality, nationalityReg);
+            userInstance.addProperty(userSex, genderReg);
+            userInstance.addProperty(userLocation, districtReg);
+            userInstance.addProperty(hasRacetype, raceTypeReg);
+            userInstance.addProperty(userTypeOfEvent, typeofEventReg);
+            userInstance.addProperty(userEventPrice, priceReg);
+            userInstance.addProperty(hasOrganization, organizationReg);
+            userInstance.addProperty(userActivityArea, activityAreaReg);
+            userInstance.addProperty(userStandardEvent, standardReg);
+            userInstance.addProperty(userLevelEvent, levelReg);
+            userInstance.addProperty(userStartPeriod, startPeriodReg);
+            userInstance.addProperty(userReward, rewardReg);
+
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            String hashedPassword = encoder.encode(plainPassword);
+
+            userInstance.addProperty(password, hashedPassword);
+
+
+            try (FileOutputStream out = new FileOutputStream(ontologyPath)) {
+                m.write(out, "RDF/XML");
+                System.out.println(userInstance);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Number of statements in OntModel: " + m.size());
+            response.setStatus("Success");
+
+            return response;
+        }
+        else {
+            response.setStatus("Error: Username already exists");
+            return response;
+        }
     }
 
 }
